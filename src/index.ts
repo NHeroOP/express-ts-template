10a82204b4c2a9267e9c7a7613a9f471bd0f95ee
NHeroOP <79276833+NHeroOP@users.noreply.github.com>
2026-05-09 13:40:49 +0530

    feat: add dotenv for environment variable management and restructure server initialization

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import express from "express";
+import "dotenv/config";
+import { connectDB } from "./config/connectDB.js";
+import { app } from "./app.js";
 
-const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  console.log(`https://localhost:${PORT}`);
-})
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`)
+    })
+  })
+  .catch((err) => {
+    console.log("DB Connection Err", err);
+    process.exit(1);
+  });
